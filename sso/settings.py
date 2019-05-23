ab3eff3eabe4eaafa8f32cc8c133086f4abf6f4e
lixiang@kanzhun.com <lixiang@kanzhun.com>
2019-05-23 16:14:33 +0800

    1.解决跨域问题 2.解决page_size警告

--- sso/settings.py
+++ sso/settings.py
@@ -58,9 +58,9 @@ MIDDLEWARE = [
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-    '*',
-)
+# CORS_ORIGIN_WHITELIST = (
+#     '*',
+# )
 CORS_ALLOW_METHODS = (
     'DELETE',
     'GET',
@@ -154,6 +154,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
 
