206e7df9df4d2daf2d7f147b688ca2a7f55883f1
rongjunfeng@cmcm.com <rongjunfeng@cmcm.com>
2019-06-10 22:12:17 +0800

    fix add user err

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
     """
     username    = serializers.CharField(required=False, read_only=False, max_length=32, label="用户名", help_text="用户名")
     name        = serializers.CharField(required=False, read_only=False, label="姓名", help_text="姓名")
-    is_active   = serializers.BooleanField(required=False, label="登陆状态", help_text="登陆状态")
+    is_active   = serializers.BooleanField(required=False, read_only=True, label="登陆状态", default=True, help_text="登陆状态")
     email       = serializers.CharField(read_only=True, help_text="联系邮箱")
     last_login  = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True, help_text="上次登录时间")
     phone       = serializers.CharField(required=False, max_length=11, min_length=11, allow_null=True, help_text="手机号",
