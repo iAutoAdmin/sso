ffc4926d1d3d1dcae8b55b9cfdf718464a4b7a21
lixiang@kanzhun.com <lixiang@kanzhun.com>
2019-06-23 22:24:05 +0800

    fix filter error

--- apps/users/filters.py
+++ apps/users/filters.py
@@ -10,12 +10,16 @@ class UserFilter(django_filters.rest_framework.FilterSet):
     """
     用户过滤类
     """
-    username = django_filters.CharFilter(method='search_username')
-    name = django_filters.CharFilter(method='search_username')
-    is_active = django_filters.CharFilter(method='search_username')
+    # username = django_filters.CharFilter(method='search_username')
+    # name = django_filters.CharFilter(method='search_username')
+    # is_active = django_filters.CharFilter(method='search_username')
+    #
+    # def search_username(self, queryset, name, value):
+    #     return queryset.filter(Q(name__icontains=value) | Q(username__icontains=value) | Q(is_active=value))
 
-    def search_username(self, queryset, name, value):
-        return queryset.filter(Q(name__icontains=value) | Q(username__icontains=value) | Q(is_active=value))
+    username = django_filters.CharFilter(lookup_expr='icontains', help_text='请输入用户名')
+    name = django_filters.CharFilter(lookup_expr='icontains', help_text='请输入中文名')
+    is_active = django_filters.CharFilter(lookup_expr='icontains', help_text='状态')
 
     class Meta:
         model = User
