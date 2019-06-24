e0a05415eb11bd3d4aeb54bf9fc5c91ee809d17a
lixiang@kanzhun.com <lixiang@kanzhun.com>
2019-06-24 19:45:44 +0800

    fix paginations

--- sso/settings.py
+++ sso/settings.py
@@ -155,7 +155,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'DEFAULT_PAGINATION_CLASS': 'sso.pagination.Pagination',
+    'DEFAULT_PAGINATION_CLASS': 'sso.paginations.Pagination',
     'PAGE_SIZE': 10
 }
 
